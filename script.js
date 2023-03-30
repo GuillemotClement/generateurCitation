f518c2d78039ab3f95918a0e2c17148404525004
GuillemotClement <guillemot.clement@protonmail.com>
2023-03-30 14:37:29 +0200

    Ajout du tableau des citations

---
+++ script.js
@@ -0,0 +1,13 @@
+//TABLEAU DES CITATION
+let mesCitations = [
+    ["On ne se baigne jamais deux fois dans le même fleuve", "Héraclite"],
+    ["Je sais que je ne sais rien", "Socrate"],
+    ["L’homme est un animal politique ", "Aristote"],
+    [" La mort n’est rien pour nous", "Epicure"],
+    ["Je pense, donc je suis", "René Descartes"],
+    ["L’homme est né libre, et partout il est dans les fers", "Jean-Jacques Rousseau"],
+    ["Ose savoir !", "Emmanuel Kant"],
+    ["Les philosophes n'ont fait qu'interpréter le monde de différentes manières, ce qui importe, c’est de le transformer", "Karl Marx"],
+    ["L’existence précède l’essence", "Jean-Paul Sartre"],
+    ["On ne naît pas femme : on le devient", "Simone de Beauvoir"]
+];
